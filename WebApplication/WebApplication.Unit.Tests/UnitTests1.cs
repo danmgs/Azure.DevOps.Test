b13744a9e59c99121520b4163f5ec306130f66ca
danmgs <dnlnguyen@gmail.com>
2020-07-11 16:00:07 +0200

    add xunit tests

---
+++ WebApplication/WebApplication.Unit.Tests/UnitTests1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace WebApplication.Unit.Tests
+{
+    public class UnitTests1
+    {
+        [Fact]
+        public void ShoudBeNull()
+        {
+            List<string> products = null;
+            // making it failed
+            Assert.Null(products);
+        }
+
+        [Fact]
+        public void ShoudBeEqual()
+        {
+            List<string> products = null;
+            // making it failed
+            Assert.Equal(1, 1);
+        }
+    }
+}
