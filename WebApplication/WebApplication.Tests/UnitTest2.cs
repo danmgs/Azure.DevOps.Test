22c6cbe06909420d34b2abdf9fabd1b5d1df8e77
danmgs <dnlnguyen@gmail.com>
2020-07-11 14:21:24 +0200

    first commit

---
+++ WebApplication/WebApplication.Tests/UnitTest2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApplication.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(1, 2);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
---
+++ WebApplication/WebApplication.Tests/UnitTest3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApplication.Tests
+{
+    [Ignore]
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Assert.AreEqual(1, 2);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
