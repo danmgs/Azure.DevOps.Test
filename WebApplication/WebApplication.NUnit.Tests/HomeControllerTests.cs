41b496b24c8f8e30af61d4278c3fda8c31bffd8e
danmgs <dnlnguyen@gmail.com>
2020-07-24 00:47:28 +0200

    Add tests

---
+++ WebApplication/WebApplication.NUnit.Tests/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Web.Mvc;
+using WebApplication.Controllers;
+
+namespace WebApplication.NUnit.Tests
+{
+    [TestFixture]
+    [Category("Home Controller")]
+    public class HomeControllerTests
+    {        
+        [Test]
+        public void TestIndexReturnsNotNull()
+        {
+            HomeController ctrl = new HomeController();
+            ActionResult result = ctrl.Index();
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ WebApplication/WebApplication.NUnit.Tests/ValuesControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using WebApplication.Controllers;
+
+namespace WebApplication.NUnit.Tests
+{
+    [TestFixture]
+    [Category("Values Controller")]
+    public class ValuesControllerTests
+    {        
+        [Test]
+        public void TestGetReturnsNotNull()
+        {
+            ValuesController ctrl = new ValuesController();
+            var result = ctrl.Get();
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void TestDelete()
+        {
+            ValuesController ctrl = new ValuesController();
+            ctrl.Delete(1);
+            Assert.Pass();
+        }
+
+        
+    }
+}
